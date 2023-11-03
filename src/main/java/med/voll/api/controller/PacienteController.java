ea500b04ea53610c3a3009ccccf6b78fc72f3e2f
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2023-11-03 00:18:37 -0300

    feat: implementing method list patients

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,9 +2,13 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +19,15 @@ public class PacienteController {
     @Autowired
     private PacienteRepository repository;
 
-    @PostMapping
+    @PostMapping("/cadastrarMedico")
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
-
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping("/listarPacientes")
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
 }
