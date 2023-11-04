6a1715a88fa239685286fed98be235b487696636
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2023-11-04 16:07:29 -0300

    refactor: added ResponseEntity to make right HTTP requests

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("medicos")
@@ -21,10 +23,14 @@ public class MedicoController {
     @Transactional
     // This annotation allows active transactions with database, since I'm entering values. From spring. When I want add or update some information in the database.
     // This "@Valid" is essencial to execute Bean Validation.
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+        repository.save(medico);
 
-        //Instantiate the object and saving in
-        repository.save(new Medico(dados));
+        var uri = uriBuilder.path("medicos/cadastrarMedico/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhadosMedico(medico));
+        // the POST method needs to return code 201 (created), header location with uri and in the response body it needs to return the thing created
     }
 
 //    @GetMapping("/listarMedicos")
@@ -34,28 +40,39 @@ public class MedicoController {
 
     @GetMapping("/listarMedicos")
     // // If there are parameters directly in the url, the configuration below in PageableDefault is overwritten.
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable paginacao) {
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable paginacao) {
         // sort ascending (default) -> http://localhost:8080/medicos/listarMedicos?sort=nome or http://localhost:8080/medicos/listarMedicos?sort=crm
         // sort descending -> http://localhost:8080/medicos/listarMedicos?sort=email,desc
-        // pagination -> http://localhost:8080/medicos/listarMedicos?size=10&page=2 or
-        http:
-//localhost:8080/medicos/listarMedicos?size=20&page=0
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        // pagination -> http://localhost:8080/medicos/listarMedicos?size=10&page=2 or http://localhost:8080/medicos/listarMedicos?size=20&page=0
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/detalharMedico/{id}")
+    public ResponseEntity detalhar(@PathVariable long id) {
+        var medico = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhadosMedico(medico));
     }
 
     @PutMapping("/atualizarMedico")
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhadosMedico(medico));
+        // create another DTO because it is not recommended to return the JPA entity in the controller
     }
 
     @DeleteMapping("/deletarMedico/{id}")
     @Transactional
-    public void deletar(@PathVariable long id) { // this way I inform to spring that this paramter is the same in path above
-//        repository.deleteById(id); // really delete from database and system
-
+    public ResponseEntity deletar(@PathVariable long id) { // this way I inform to spring that this paramter is the same in path above
         repository.getReferenceById(id).desativarMedico(); // logic delete
+
+        return ResponseEntity.noContent().build(); // more appropriate is request 204, as the requisition has been processed and has no return arguments
+        // noContent() creates an object and we call build() to build the ResponseEntity object.
     }
 
 }
